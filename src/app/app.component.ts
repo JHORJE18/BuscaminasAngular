3cc32a05c93cfe0f5ccf16c995b897ede23e485d
Jorge <wiijlg#hotmail.com>
2019-01-21 21:05:49 +0100

    Pantalla principal y tabla
    Pantalla con botón donde el usuario inicia el juego.
    Tambien se cuenta con una función donde genera una tabla según el número de piezas especificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BuscaminasAngular';
+	// Variables
+	title = 'BuscaminasAngular';
+	name = "";
+	contenido: String = "";
+
+	// Link
+	@ViewChild('contenido') contenedor: ElementRef;
+	@ViewChild('btnIniciar') btnIniciar: ElementRef;
+
+	StartGameEvent() {
+		var name = prompt("Introduce tu nombre");
+		var piezas = prompt("Introduce el número de piezas");
+
+		// Oculta botón Iniciar partida
+		this.btnIniciar.nativeElement.style.display = "none";
+
+		this.PreparePlantilla(piezas);
+	}
+
+	/**
+	 * Metodo prepara la interfaz de la tabla
+	 * @param {int} piezas Recibe el número de piezas que se mostraran
+	 */
+	PreparePlantilla(piezas) {
+		var tableBase = document.createElement('table');
+		var tbody = document.createElement('tbody');
+
+		for (var i = 0; i<piezas; i++){
+			var fila = document.createElement('tr');
+
+			for (var c = 0; c< piezas; c++){
+				var columna = document.createElement('td');
+				columna.style = "background-color: red; border: 2px solid white; width: 20px; height: 20px;";
+
+				fila.appendChild(columna);
+			}
+
+			tbody.appendChild(fila);
+		}
+		tableBase.appendChild(tbody);
+		this.contenedor.nativeElement.appendChild(tableBase);
+	}
 }
