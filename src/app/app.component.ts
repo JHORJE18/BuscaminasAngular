717441c960483234eee58eb73054cae0dcebbdd4
Jorge <wiijlg#hotmail.com>
2019-01-23 17:31:32 +0100

    -- ULTIMA OPCIÓN CON ARRAY 2 DIMENSIONES --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 	title = 'BuscaminasAngular';
 	name = "";
 	contenido: String = "";
-	arrayPiezas = [[], []]
+	arrayPiezas = [];
 
 	// Link
 	@ViewChild('contenido') contenedor: ElementRef;
@@ -27,8 +27,33 @@ export class AppComponent {
 	}
 
 	StartGameEvent() {
-		var name = prompt("Introduce tu nombre");
-		var piezas = prompt("Introduce el número de piezas");
+		// Obtenemos valores del Nombre y la dificultad seleccionada
+		// TODO: Cargar valores y no usar uno predeteminado
+		var nombre = "Jorge";
+		var dificultad = 1;
+		var piezas;
+
+		switch (dificultad) {
+			case 0:
+				// Facil
+				piezas = 16;
+				break;
+			case 1:
+				// Normal
+				piezas = 25;
+				break;
+			case 2:
+				// Dificil
+				piezas = 36;
+				break;
+		}
+
+		// Inicializamos Array
+		var arrayPiezas = new Array(piezas)
+		arrayPiezas = colocarBombas(arrayPiezas, 5);
+		console.log(arrayPiezas);
+
+
 
 		// Oculta botón Iniciar partida
 		this.btnIniciar.nativeElement.style.display = "none";
@@ -63,7 +88,35 @@ export class AppComponent {
 		tableBase.appendChild(tbody);
 		this.contenedor.nativeElement.appendChild(tableBase);
 	}
+}
 
+/**
+ * Metodo genera array de 2 dimensiones con el que se manipulara todo
+ * @param numPiezas Recibe el número de piezas con las que se va a jugar
+ * @returns {array} Array con valores 0 de dos dimensiones
+ */
+function iniciarArray(numPiezas) {
+	// Calculamos cuadrado perfecto 
+	let cuadrado = Math.sqrt(numPiezas);
+
+	var nuevoArray = new Array(cuadrado);
+	//Bucle para meter en cada posición otros array de 10
+	for (var i = 0; i < numPiezas; i++) {
+		nuevoArray[i] = new Array(2);
+	}
+
+	console.log(nuevoArray);
+
+	// Genera cada posición de celda
+	for (let i = 0; i < numPiezas; i++) {
+		// Genera cada cordenada correspondiente
+		for (let c = 0; c < 2; c++) {
+			nuevoArray[i][c] = 0;
+		}
+	}
+
+	console.log(nuevoArray);
+	return nuevoArray;
 }
 
 /**
@@ -90,10 +143,12 @@ function colocarBombas(arrayPiezas, numBombas) {
 		posicionesbombas.push(aleatorio);
 	}
 
+	console.log("Coloco bombas en las posiciones: " + posicionesbombas);
+
 	// Colocar bombas
 	for (var i = 0; i < posicionesbombas.length; i++) {
-		var cordenadabomba = cordenadaPieza(posicionesbombas[i],arrayPiezas.length)
-
+		var cordenadabomba = cordenadaPieza(posicionesbombas[i], arrayPiezas.length)
+		console.log("La posición " + posicionesbombas[i] + " corresponde a la posición " + cordenadabomba);
 		arrayPiezas[cordenadabomba[0]][cordenadabomba[1]] = -1;
 	}
 
@@ -122,39 +177,36 @@ function cordenadaPieza(numPieza, totalPiezas) {
 
 
 /**
- * 
- * @param piezas (el array bidimensional que contiene las casillas donde hay una bomba)
  * EL metodo comprueba cuantas bombas hay alrededor de cada pieza y lo registra con un numero.
+ * @param piezas (el array bidimensional que contiene las casillas donde hay una bomba)
+ * @returns {Array[Piezas]} Devuelve un array de 2 dimensiones con todas los valores resultantes
  */
-function checkBombs (piezas){
+function checkBombs(piezas) {
 	//He tardado como 6-7 minutos, si esto funciona me vuelvo creyente :3
 
-	for(var i=0; i<piezas.lenght; i++){
+	for (var i = 0; i < piezas.lenght; i++) {
 
-		for(var x=0; x<piezas[i].lenght; x++){
+		for (var x = 0; x < piezas[i].lenght; x++) {
 			var counter;
 
-			if(piezas[i][x] != -1){
-				if(piezas[i-1][x] != undefined && piezas[i-1][x] == -1){counter++;}
-				if(piezas[i-1][x+1] != undefined && piezas[i-1][x+1] == -1){counter++;}
-				if(piezas[i][x+1] != undefined && piezas[i][x+1] == -1){counter++;}
-				if(piezas[i+1][x+1] != undefined && piezas[i+1][x+1] == -1){counter++;}
-				if(piezas[i+1][x] != undefined && piezas[i+1][x] == -1){counter++;}
-				if(piezas[i+1][x-1] != undefined && piezas[i+1][x-1] == -1){counter++;}
-				if(piezas[i][x-1] != undefined && piezas[i][x-1] == -1){counter++;}
-				if(piezas[i-1][x-1] != undefined && piezas[i-1][x-1] == -1){counter++;}
-			}else{
-
+			if (piezas[i][x] != -1) {
+				if (piezas[i - 1][x] != undefined && piezas[i - 1][x] == -1) { counter++; }
+				if (piezas[i - 1][x + 1] != undefined && piezas[i - 1][x + 1] == -1) { counter++; }
+				if (piezas[i][x + 1] != undefined && piezas[i][x + 1] == -1) { counter++; }
+				if (piezas[i + 1][x + 1] != undefined && piezas[i + 1][x + 1] == -1) { counter++; }
+				if (piezas[i + 1][x] != undefined && piezas[i + 1][x] == -1) { counter++; }
+				if (piezas[i + 1][x - 1] != undefined && piezas[i + 1][x - 1] == -1) { counter++; }
+				if (piezas[i][x - 1] != undefined && piezas[i][x - 1] == -1) { counter++; }
+				if (piezas[i - 1][x - 1] != undefined && piezas[i - 1][x - 1] == -1) { counter++; }
+			} else {
 				counter = -1
-
 			}
 
 			piezas[i][x] = counter;
 		}
-	
+
 
 	}
 
 	return piezas;
-
 }
